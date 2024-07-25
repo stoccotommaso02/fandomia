59ea97ff2f78bd41916a0ae5c3e4ee46ae96c18b
josephgrant26 <joseph@englishand.it>
2024-07-25 19:01:17 +0200

    aggiunto i drop statements,  cambiato i nomi degli attributi 'release' e 'product' perche keyword riservate del linguaggio.

--- dbschema.sql
+++ dbschema.sql
@@ -1,62 +1,66 @@
-CREATE TABLE users(
-  username VARCHAR(25) NOT NULL, -- forse e-mail
-  password VARCHAR(30) NOT NULL,
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS videogames;
+DROP TABLE IF EXISTS comics;
+DROP TABLE IF EXISTS music;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
 
+CREATE TABLE users (
+  username VARCHAR(25) NOT NULL,
+  password VARCHAR(30) NOT NULL,
   PRIMARY KEY (username)
 );
 
-CREATE TABLE products(
+CREATE TABLE products (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) UNIQUE NOT NULL,
-  price decimal(10,2) UNSIGNED,
-  release DATE,
-  type ENUM('book', 'music', 'comic', 'videogame') NOT NULL,
+  price DECIMAL(10,2) UNSIGNED,
+  release_date DATE,
+  product_type ENUM('book','music','comic','videogame') NOT NULL,
   status ENUM('available', 'not available'),
-
   PRIMARY KEY (id)
 );
 
-CREATE TABLE books(
-  id int UNSIGNED NOT NULL,
-  author varchar(50) NOT NULL,
-  pages int UNSIGNED,
-  genre varchar(50),
-
+CREATE TABLE books (
+  id INT UNSIGNED NOT NULL,
+  author VARCHAR(50) NOT NULL,
+  pages INT UNSIGNED,
+  genre VARCHAR(50),
   PRIMARY KEY(id),
   FOREIGN KEY(id) REFERENCES products(id)
 );
 
-CREATE TABLE music(
-  id int UNSIGNED NOT NULL,
-  artist varchar(50) NOT NULL,
-  length int unsigned, --in minutes
-  genre varchar(50),
+CREATE TABLE music (
+  id INT UNSIGNED NOT NULL,
+  artist VARCHAR(50) NOT NULL,
+  length INT UNSIGNED, -- in minutes
+  genre VARCHAR(50),
   format ENUM('cd', 'vinyl', 'other'),
-
   PRIMARY KEY(id),
   FOREIGN KEY(id) REFERENCES products(id)
-)
+);
 
-CREATE TABLE comics(
-  id int UNSIGNED NOT NULL,
-  author varchar(50) NOT NULL,
-  pages int unsigned,
-  genre varchar(50),
-)
+CREATE TABLE comics (
+  id INT UNSIGNED NOT NULL,
+  author VARCHAR(50) NOT NULL,
+  pages INT UNSIGNED,
+  genre VARCHAR(50),
+  PRIMARY KEY(id),
+  FOREIGN KEY(id) REFERENCES products(id)
+);
 
-CREATE TABLE videogames(
+CREATE TABLE videogames (
   id INTEGER UNSIGNED NOT NULL,
-  genre varchar(50),
-  developer varchar(50),
-
+  genre VARCHAR(50),
+  developer VARCHAR(50),
   PRIMARY KEY(id),
   FOREIGN KEY(id) REFERENCES products(id)
-)
+);
 
-CREATE TABLE wishlist(
-  user_username varchar(25) NOT NULL,
-  product_id int UNSIGNED NOT NULL,
-
+CREATE TABLE wishlist (
+  user_username VARCHAR(25) NOT NULL,
+  product_id INT UNSIGNED NOT NULL,
   PRIMARY KEY(user_username, product_id),
   FOREIGN KEY(user_username) REFERENCES users(username),
   FOREIGN KEY(product_id) REFERENCES products(id)
