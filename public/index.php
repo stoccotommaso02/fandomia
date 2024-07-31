dce1340cfbfa468c70ca1e98069171c948c13c0e
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-07-31 14:40:04 +0200

    modificato index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,50 @@
 <?php 
 
+require_once("buildHeader.php");
 //Servono :
 //-una funzione/classe per compilare il template della card di ogni prodotto;
 //-una funzione/classe per compilare ogni section presente in homePage;
 //-una classe che compila l'header;
 //-una classe che compila il footer;
 //-una classe per dis/connettersi al DB;
+$latestItems = getLatestItems();
+$latestSection = file_get_contents("../templates/section.html");
+$latestSection = str_replace('{{listaProdotti}}',$latestItems,$latestSection);
 
-$jsonFile=file_get_contents("../assets/libri.json");
+$nextItems = getNextItems();
+$nextSection = file_get_contents("../templates/section.html");
+$nextSection = str_replace('{{nextItems}}',$nextItems,$nextSection);
+
+$saleItems = getSaleItems();
+$saleSection = file_get_contents("../templates/section.html");
+$saleSection = str_replace('{{listaProdotti}}',$saleItems,$saleSection);
+
+$header = buildHeader();
+
+$homePageTemplate = file_get_contents("../templates/index.html");
+$homePageTemplate = str_replace('{{header}}',$header,$homePageTemplate);
+$homePageTemplate = str_replace('{{latestItems}}',$latestSection,$homePageTemplate);
+$homePageTemplate = str_replace('{{nextItems}}',$nextItems,$homePageTemplate);
+$homePageTemplate = str_replace('{{saleItems}}',$saleSection,$homePageTemplate);
+
+echo($homePageTemplate);
+
+function getLatestItems() : string {
+    //implementazione;
+    $latestItems = '';
+    return $latestItems;
+}
+
+function getNextItems() : string {
+    //implementazione 
+    $nextItems = '';
+    return $nextItems;
+};
+
+function getSaleItems() : string {
+    /* implementazione: per iniziare ho usato un file Json; quando avremo
+    il DB questi metodo faranno delle query.
+    $jsonFile=file_get_contents("../assets/libri.json");
 
 $jsonEncoded=json_decode($jsonFile,true);
 
@@ -23,15 +60,9 @@ foreach($jsonEncoded as $libro) {
     $libroTemplate=str_replace('{{genere}}',$libro['genere'],$libroTemplate);
     $libroTemplate = str_replace('{{productId}}',$libro['id'],$libroTemplate);
     $listaLibri .= $libroTemplate;
-}
-
-$saleSection = file_get_contents("../templates/section.html");
-$saleSection = str_replace('{{listaProdotti}}',$listaLibri,$saleSection);
-
-$homePageTemplate = file_get_contents("../templates/index.html");
-$homePageTemplate = str_replace('{{saleItems}}',$saleSection,$homePageTemplate);
-
-echo($homePageTemplate);
-//echo($indexTemplate);
+}  */
+    $saleItems = '';
+    return $saleItems;
+};
 
 ?>
