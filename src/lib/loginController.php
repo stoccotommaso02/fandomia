745dafa1013fa6657218bf398118de11ff841b48
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-08-09 17:14:35 +0200

    aggiunti errors in caso di login fallito

--- src/lib/loginController.php
+++ src/lib/loginController.php
@@ -6,23 +6,24 @@ if (isset($_POST['userEmail']))
  {
  $user = sanitizeString($_POST['userEmail']);
  $pass = sanitizeString($_POST['password']);
- if ($user == "" || $pass == "")
- $error = "Not all fields were entered<br>";
+ if ($user == "" || $pass == "") {
+ $error = "Non sono stati compilati tutti i campi!";
+ header("Location: ../login.php?error=" . urlencode($error));
+ exit();
+}
  else
  {
  $result = queryMySQL("SELECT username,password FROM Users
  WHERE username='$user' AND password='$pass'");
  if ($result->num_rows == 0)
  {
- $error = "<span class='error'>Username/Password
- non validi</span><br><br>";
- $loginTemplate = file_get_contents('../templates/login.html');
- $loginTemplate = str_replace('{{errors}}',$error,$loginTemplate);
- header("Location: ../login.php");
+ $error = "Credenziali non corrette.";
+ header("Location: ../login.php?error=" . urlencode($error));
+ exit();
  }
  else
  {
-    session_start();
+ session_start();
  $_SESSION['loggedUser'] = $user;
  header("Location: ../index.php");
  }
@@ -42,5 +43,13 @@ if (isset($_POST['userEmail']))
     $result = $connection->query($query);
     return $result;
  }
-
+/* La prof in classe ha detto che sarebbe bene, quando ci si logga/registra, essere reindirizzato
+non di default alla homePage, ma alla pagina in cui si era prima; decidere se farlo! Il modo che ho trovato è che
+venga salvato $_SESSION['last_page'] = $_SERVER['REQUEST_URI'] e poi :
+if (isset($_SESSION['last_page'])) {
+            $redirect_url = $_SESSION['last_page'];
+            unset($_SESSION['last_page']); // Rimuove la variabile per evitare reindirizzamenti non voluti in futuro
+        } 
+        header("Location: $redirect_url");
+        exit(); */
 ?>
--- src/login.php
+++ src/login.php
@@ -1,6 +1,12 @@
 <?php 
 $header = '';
 require_once("buildHeader.php");
+
+if (isset($_GET['error'])) {
+    $errorMessage = '<p style="color:red;">' . htmlspecialchars($_GET['error']) . '</p>';
+} else {
+    $errorMessage = '';
+}
 if (session_start() && isset($_SESSION['loggedUser']))
    { $header = $_SESSION['loggedUser'];}
 else 
@@ -8,6 +14,7 @@ else
 
 $loginTemplate = file_get_contents("./templates/login.html");
 $loginTemplate = str_replace('{{header}}',$header,$loginTemplate);
+$loginTemplate = str_replace('{{errors}}',$errorMessage,$loginTemplate);
 
 echo($loginTemplate);
 
