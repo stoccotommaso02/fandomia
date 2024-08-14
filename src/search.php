ca865188ee0aa11bd311a33c4b7cb7e356b2584f
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-08-14 14:15:44 +0200

    codificata prima logica della ricerca di un prodotto

--- src/search.php
+++ src/search.php
@@ -1,2 +1,46 @@
 <?php 
+
+require_once("./lib/global.php");
+require_once("./lib/DBController.php");
+require_once("header.php");
+require_once("footer.php");
+
+$searchParam = '';
+if (isset($_GET['search'])) {
+    $searchParam = $_GET['search'];
+    unset($_GET['search']);
+}
+
+$header = buildHeader();
+$footer = buildFooter();
+
+$searchResult = searchList($searchParam);
+
+$searchTemplate = new Template();
+$searchTemplate = $searchTemplate -> render('searchResult.html',array("header" => $header,
+                                                                      "footer" => $footer,
+                                                                      "searchList" => $searchResult));
+                 
+echo ($searchTemplate);
+
+function searchList(string $searchParam) : string {
+    $connection = new DBconnection;
+    $connection -> setConnection();
+    
+    $searchQuery = "SELECT *
+                    from Products
+                    where name = '$searchParam'";
+    
+    $results = $connection -> queryDB($searchQuery);
+    
+    $searchResult = '';
+    if (empty($results)) {
+        $searchResult = "Nessun prodotto corrisponde alla tua ricerca!";
+    } else {
+    foreach($results as $result) {
+        $searchResult .= $result['name'];
+        }
+    }
+    return $searchResult;
+}
 ?>
