643e8b1f65d4a692a6d1d1cf9869f98e0702dd4a
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-08-28 10:21:06 +0200

    tolto form filtri dalla pagina delle singole categorie

--- src/products_page.php
+++ src/products_page.php
@@ -20,13 +20,6 @@ switch($category) {
         break;
     case "videogame":
         $genre_table = $products = "Videogames";
-        $extra_filters = '<fieldset>
-                            <legend>Consolle:</legend>
-                            <label><input type="checkbox" name="platform[]" value="Playstation"> Playstation</label><br>
-                            <label><input type="checkbox" name="platform[]" value="Nintendo"> Nintendo</label><br>
-                            <label><input type="checkbox" name="platform[]" value="Pc"> PC</label><br>
-                            <label><input type="checkbox" name="platform[]" value="Xbox"> Xbox</label><br>
-                          </fieldset>';
         break;
     case "book":
         $genre_table = "Books";
@@ -35,75 +28,8 @@ switch($category) {
     case "music":
         $genre_table = "Music";
         $products = "Musica";
-        $extra_filters = '<fieldset>
-                            <legend>Formato:</legend>
-                            <label><input type="radio" name="format" value="cd"> CD</label><br>
-                            <label><input type="radio" name="format" value="other"> Formato misterioso</label><br>
-                            <label><input type="radio" name="format" value="vinyl"> Vinile</label><br>
-                          </fieldset>';
         break;
 }
-$sql = "SELECT * FROM Products join $genre_table on Products.id = {$genre_table}.id ";
-
-// Array per contenere i valori dei parametri da bindare
-$params = [];
-$types = ""; // Stringa per i tipi di dato (es. "ssi" per stringa, stringa, intero)
-
-if (isset($_GET['category'])) {
-    $sql .= "WHERE product_type = ?";
-    $types .= "s"; // Tipo stringa per categoria
-    $params[] = $_GET['category'];
-    }  else {
-        header("Location:index.php");
-        exit();
-    }
-/* si possono selezionare al momento più prezzi*/
-if(!empty($_GET['price'])){
-
-    $prices = $_GET['price'];
-    $prices_conditions = array();
-
-    foreach($prices as $price_filter) {
-        list($min, $max) = explode('-', $price_filter);
-        $prices_conditions[] = "price BETWEEN ? and ?";
-        $types .= "ii"; 
-        $params[] = $min;
-        $params[] = $max;
-    }
-    if (!empty($prices_conditions)) {
-        $sql .= " AND (" . implode(" OR ", $prices_conditions) . ")";
-    }
-}
-/* lo sconto al momento dovrebbe essere un radiobox*/
-if (isset($_GET['sale_percentage'])) {
-    list($min, $max) = explode('-', $_GET['sale_percentage']);
-    $sql .= " AND (sale_percentage BETWEEN ? AND ?)";
-    $types .= "ii"; 
-    $params[] = $min;
-    $params[] = $max;
-}
-/* Il genre deve essere un radiobox */
-if(isset($_GET['genre'])) {
-
-    $genre_checked = $_GET['genre'];
-    $types .= "s"; // Tipo intero per prezzo minimo e massimo
-    $params[] = $genre_checked;
-    $sql .= " AND genre = ?";
-}
-
-// Prepara la query
-$connection = new DBconnection();
-$connection -> setConnection();
-
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$stmt = $connection->prepare($sql);
-// Bind dei parametri se esistono
-if (!empty($params)) {
-    $stmt->bind_param($types,...$params);
-}
-// Esegui la query
-$stmt->execute();
-$result = $stmt->get_result();
 $products_list = '';
 
 $products_per_page = 10; // Numero di prodotti per pagina
@@ -114,8 +40,19 @@ $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 // Calcola l'offset (da dove iniziare a prendere i prodotti)
 $offset = ($page - 1) * $products_per_page;
 
+$query = "SELECT * 
+        FROM Products join $genre_table on Products.id = {$genre_table}.id
+        order by name 
+        limit {$products_per_page}
+        offset {$offset}  ";
+
+$connection = new DBconnection();
+$connection -> setConnection();
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$result = $connection -> queryDB($query);
 // Verifica se ci sono prodotti che corrispondono ai filtri
-if ($result->num_rows > 0) {
+if (!empty($result)) {
     $products_list .= '<ul class="products_list">';
     // Mostra i prodotti filtrati
     foreach ($result as $row) {
@@ -125,15 +62,33 @@ if ($result->num_rows > 0) {
         }
     $products_list .= '</ul>';
 } else {
+     //Da inserire un possibile errore 404;
      $products_list .= "Nessun prodotto corrispondente filtri selezionati";
 }
-
-$total_products_result = $result -> num_rows;
-$total_pages = $total_products_result / $products_per_page;
+//Per la paginazione, devo conoscere il totale di prodotti della particolare categoria
+$total_products_query = "SELECT COUNT(*) as total_products
+                         FROM Products join $genre_table on Products.id = {$genre_table}.id";
+$connection -> setConnection();
+$result = $connection -> queryDB($total_products_query);
+$total_products ;
+if (!empty($result))    {
+    $total_products = $result[0]['total_products'];
+}   else {
+    //stesso discorso di prima?
+}
+$total_pages = $total_products / $products_per_page;
 $total_pages = ceil($total_pages);
 
 // Visualizza i link di paginazione
 $pagination_links =  "<div class='pagination'>";
+$next = $previous = "";
+
+    if ($page > 1) {
+        $previous = "<a href='products_page.php?page=" . ($page - 1) . "&category=$category'" . ">Previous</a>";
+    } else {
+        $previous = "<span>Previous</span> "; // Disabled state
+    }
+$pagination_links .= $previous;
 for ($i = 1; $i <= $total_pages; $i++) {
     if ($i == $page) {
         $pagination_links .= "<strong>$i</strong> "; // Pagina corrente senza link
@@ -142,29 +97,20 @@ for ($i = 1; $i <= $total_pages; $i++) {
     }
 }
 
-//Query per recuperare i generi della particolare tipologia di prodotto
-$connection -> setConnection();
-$query = "SELECT DISTINCT genre from $genre_table";
-$result = $connection -> queryDB($query);
-$genre_list = '';
-// Verifica se ci sono prodotti che corrispondono ai filtri
-if (!empty($result)) {
-    // Mostra i prodotti filtrati
-    foreach ($result as $row) {
-            $genre_list .= '<label><input type="radio" name="genre" value="' . $row['genre'] . '">' . $row['genre'] . "</label><br>";
-        }
-}   
+if ($page < $total_pages) {
+    $next = "<a href='products_page.php?page=" . ($page + 1) . "&category=$category'" . ">Next</a>";
+} else {
+    $next = "<span>Next</span>"; // Disabled state
+}
+$pagination_links .= $next;
+$pagination_links .=  "</div>";
 
 $products_page_template = new Template();
 $products_page_template = $products_page_template->render("products_page.html",array("header" => $header,
                                                                                      "category" => $_GET['category'],
                                                                                      "products" => $products,
-                                                                                     "genre_list" => $genre_list,
-                                                                                     "extra_filters" => $extra_filters,
                                                                                      "products_list" => $products_list,
                                                                                      "pagination_links" => $pagination_links,
                                                                                      "footer" => $footer));
 echo($products_page_template);
-
-$stmt->close();
 ?>
