d780a7daa009f3297e6a2cfff6c714884806d916
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-09-02 14:27:50 +0200

    paginazione resa accessibile,credo

--- src/pagination_links_factory.php
+++ src/pagination_links_factory.php
@@ -8,37 +8,51 @@ $total_pages = $total_products / $products_per_page;
 $total_pages = ceil($total_pages);
 
 // Visualizza i link di paginazione
-$pagination_links =  "<div class='pagination'>";
+$pagination_links =  "<nav role='navigation' aria-label='Pagination Navigation'><ul>";
 $next = $previous = "";
 
     if ($page > 1) {
-        $previous = "<a href='?page=" . ($page - 1) .
+        $previous = "<li><a href='?page=" . ($page - 1) .
                     ($category != null? "&category=$category": '') .
-                    "'>Precedente</a>";
+                    "' aria-label='Vai alla pagina precedente,Pagina " . ($page-1) . "'>Precedente</a></li>";
     } else {
         $previous = "<span>Precedente</span> "; // Disabled state
     }
 $pagination_links .= $previous;
 for ($i = 1; $i <= $total_pages; $i++) {
     if ($i == $page) {
-        $pagination_links .= "<strong>$i</strong> "; // Pagina corrente senza link
+        $pagination_links .= "<li><a href='/page=$i' aria-label='Pagina attuale,Pagina $i' aria current='true'><strong>$i</strong></a></li>"; // Pagina corrente senza link
     } else {
-        $pagination_links .= "<a href='?page=$i" . 
+        $pagination_links .= "<li><a href='?page=$i aria-label='Vai a pagina $i'" . 
                              ($category != null? "&category=$category": '') .
-                             "'>$i</a> "; // Altre pagine con link
+                             "'>$i</a></li> "; // Altre pagine con link
     }
 }
 
 if ($page < $total_pages) {
-    $next = "<a href='?page=" . ($page + 1) .
+    $next = "<li><a href='?page=" . ($page + 1) .
             ($category != null? "&category=$category": '') .
-            "'>Prossimo</a>";
+            "' aria-label='Vai alla pagina successiva,Pagina " . ($page+1) . "'>Prossimo</a></li>";
 } else {
     $next = "<span>Prossimo</span>"; // Disabled state
 }
 $pagination_links .= $next;
-$pagination_links .=  "</div>";
+$pagination_links .=  "</ul>";
 
 return $pagination_links;
 }
 ?>
+
+<li>
+          <a href="/page-3" aria-label="Current Page, Page 3" aria-current="true">3</a>
+        </li>
+
+        <nav role="navigation" aria-label="Pagination Navigation">
+    <ul>
+        <li><a href="/page-1" aria-label="Goto Page 1">1</a></li>
+        <li><a href="/page-2" aria-label="Goto Page 2">2</a></li>
+        <li><a href="/page-3" aria-label="Goto Page 3">3</a></li>
+        <li><a href="/page-4" aria-label="Goto Page 4">4</a></li>
+        <li><a href="/page-5" aria-label="Goto Page 5">5</a></li>
+    </ul>
+</nav>
